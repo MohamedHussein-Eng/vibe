53c8eb90f64b62f3f780bce10047cc57458e67b4
Mohamed Hussein <mohamedhusseinelsaid11@gmail.com>
2026-08-22 17:43:03 +0300

    handle image commit

--- src/comps/Profile/PostsSaved.jsx
+++ src/comps/Profile/PostsSaved.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
-import { post_image } from '../../images/PostImage.png'
+import post_image from '../../images/PostImage.png'
 
 export default function MyPostsSaved({ posts, savedPosts, profile }) {
   const [activeFilter, setActiveFilter] = useState('posts')
