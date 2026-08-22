0b46d27a1560d76db040c99f4b5b460c3c35e0bf
Mohamed Hussein <mohamedhusseinelsaid11@gmail.com>
2026-08-22 18:23:55 +0300

    handle SharedCard commit

--- src/comps/PostBody.jsx
+++ src/comps/PostBody.jsx
@@ -2,32 +2,32 @@ import React from 'react';
 import { CardBody } from '@heroui/react';
 import Zoom from 'react-medium-image-zoom';
 import 'react-medium-image-zoom/dist/styles.css';
-import PostSharedCard from './postSharedCard';
+import PostSharedCard from './PostShared';
 
 
 
-export default function PostBody({ body, image,postShareData,isShare }) {
+export default function PostBody({ body, image, postShareData, isShare }) {
   return (
     <CardBody className="w-full h-auto  overflow-hidden flex flex-col">
       <p className="text-xl  font-bold text-white mb-3 shrink-0">
         {body}
       </p>
-      
+
       {image && (
         <div className="zoom-wrapper w-full relative flex-1 min-h-0">
-          <Zoom 
-            zoomImg={{ className: "w-full h-full object-contain" }} 
+          <Zoom
+            zoomImg={{ className: "w-full h-full object-contain" }}
           >
             <img
               alt="Card background"
-              className="rounded-xl w-full h-full object-cover" 
+              className="rounded-xl w-full h-full object-cover"
               src={image}
             />
           </Zoom>
         </div>
-       
+
       )}
-      {isShare&&<PostSharedCard postShareData={postShareData}/>}
+      {isShare && <PostSharedCard postShareData={postShareData} />}
     </CardBody>
   );
 }
